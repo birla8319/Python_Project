d4f2a633a9b855c023d0e514e3e23ea9bcabbb84
Ankit Birla <39473129+birla8319@users.noreply.github.com>
2019-08-24 20:41:04 +0530

    Add files via upload

---
+++ LinearRegressionUsingSciKitLibrary.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('D:\Book3.csv')
+x = np.array(df['Height']).reshape((-1, 1))
+y = np.array(df['Gender'])
+#print(x,y)
+model = LinearRegression()
+model.fit(x, y)
+#model = LinearRegression().fit(x, y)
+r_sq = model.score(x, y)
+print('coefficient of determination:', r_sq)
+print('intercept:', model.intercept_)
+print('slope:', model.coef_)
+
+new_model = LinearRegression().fit(x, y.reshape((-1, 1)))
+print('intercept:', new_model.intercept_)
+print('slope:', new_model.coef_)
+y_pred = model.predict(x)
+print('predicted response:', y_pred, sep='\n')
+y_pred = model.intercept_ + model.coef_ * x
+print('predicted response:', y_pred, sep='\n')
+x_new = np.arange(5).reshape((-1, 1))
+print(x_new)
+y_new = model.predict(x_new)
+print(y_new)
+plt.scatter(x,y)
+plt.plot(x,y_pred)
+plt.show()
